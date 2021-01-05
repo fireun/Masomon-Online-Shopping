7864c16ef34eeab980cb069bbbe9879bdbf063aa
wxun <53051768+fireun@users.noreply.github.com>
2021-01-05 18:16:35 +0800

    Update header.php

--- user/header.php
+++ user/header.php
@@ -121,6 +121,9 @@
                     
                     
                     <!-- shopping cart icon -->
+                    <?php
+                        if(isset($_SESSION['userId'])){
+                    ?>
                     <div class="cart largenav col-sm-2" id="shoppingbag">
                         <a class="cart-button" href="../user/cart.php">
                             <svg class="cart-svg " width="16 " height="16 " viewBox="0 0 16 16 ">
@@ -161,7 +164,7 @@
                                             $CartProductName = $row['name'];
                                             $CartProductPrice = $row['price'];
                                             $CartProductImage = $row['coverImage'];
-                                           
+
                                             if( strlen( $CartProductName ) > 30 ) {
                                                 $CartProductName = substr( $CartProductName, 0, 30 ) . '...';
                                             }
@@ -195,7 +198,20 @@
                                     <div class="demo" style="z-index:100;padding:20%;font-size:15px;text-align:center;">
                                         <a href="../user/login.php" style="color:#cd5c5c;font-size:25px">Login</a>
                                     </div>                            
-                            <?php } ?>
+                            <?php } 
+                            
+                        }else{
+                        ?>
+                         <div class="cart largenav col-sm-2" id="shoppingbag">
+                            <a class="cart-button" href="login.php">
+                            <svg class="cart-svg " width="16 " height="16 " viewBox="0 0 16 16 ">
+                                <path d="M15.32 2.405H4.887C3 2.405 2.46.805 2.46.805L2.257.21C2.208.085 2.083 0 1.946 0H.336C.1 0-.064.24.024.46l.644 1.945L3.11 9.767c.047.137.175.23.32.23h8.418l-.493 1.958H3.768l.002.003c-.017 0-.033-.003-.05-.003-1.06 0-1.92.86-1.92 1.92s.86 1.92 1.92 1.92c.99 0 1.805-.75 1.91-1.712l5.55.076c.12.922.91 1.636 1.867 1.636 1.04 0 1.885-.844 1.885-1.885 0-.866-.584-1.593-1.38-1.814l2.423-8.832c.12-.433-.206-.86-.655-.86 " fill="#fff "></path> <!-- cart的形状 -->
+                            </svg> Cart
+                            </a>
+                         </div>
+                        <?php
+                        }
+                        ?>
                             
                         </div><!--End shopping bag item-->
                     </div><!--End shopping cart icon-->
